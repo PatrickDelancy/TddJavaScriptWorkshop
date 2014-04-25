527ad4026e8112fe38931cb8fc796151611526de
Patrick Delancy <PDelancy@degdigital.com>
2014-04-25 00:46:46 -0500

    added istanbul for code coverage

--- gulp/tasks/runtests.js
+++ gulp/tasks/runtests.js
@@ -1,8 +1,17 @@
 var gulp	= require('gulp');
 var jasmine	= require('gulp-jasmine');
+var istanbul= require('gulp-istanbul');
 var config  = require('../config');
+var handleErrors = require('../util/handleErrors');
 
-gulp.task('runtests', function() {
-	gulp.src(config.tests)
-		.pipe(jasmine());
+gulp.task('runtests', function(cb) {
+	gulp.src(config.src.scripts)
+		.pipe(istanbul())
+		.on('end', function() {
+			gulp.src(config.tests)
+				.pipe(jasmine())
+				.on('error', handleErrors)
+		        .pipe(istanbul.writeReports('./coverage'))
+		        .on('end', cb);
+		})
 });
